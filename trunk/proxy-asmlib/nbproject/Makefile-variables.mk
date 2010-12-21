920bfccd4a70ed895807a56c06bfe273387bee27
asilichenko <asilichenko@c1ff3fa2-d151-6539-e463-ff6a09a06c30>
2010-12-21 12:55:30 +0000

    init commit

---
+++ trunk/proxy-asmlib/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=libproxy-asmlib.dll
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/libproxy-asmlib.dll
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=libproxy-asmlib.dll.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/libproxy-asmlib.dll.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=libproxy-asmlib.dll
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/libproxy-asmlib.dll
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=libproxy-asmlib.dll.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/libproxy-asmlib.dll.tar
