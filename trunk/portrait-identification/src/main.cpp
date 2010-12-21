1f2972f685f871e2a1f325a41df52224615f07d4
asilichenko <asilichenko@c1ff3fa2-d151-6539-e463-ff6a09a06c30>
2010-12-21 13:01:57 +0000

    init commit

---
+++ trunk/portrait-identification/src/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Alex Judge
+ *
+ * Created on 21 Декабрь 2010 г., 14:07
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
