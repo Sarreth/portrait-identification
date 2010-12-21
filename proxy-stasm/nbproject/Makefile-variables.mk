25dc8364d3951f1a003bf515e08bb40048a2aaa1
asilichenko <asilichenko@c1ff3fa2-d151-6539-e463-ff6a09a06c30>
2010-12-21 12:55:30 +0000

    init commit

---
+++ proxy-stasm/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=libproxy-stasm.dll
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/libproxy-stasm.dll
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=libproxy-stasm.dll.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/libproxy-stasm.dll.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=libproxy-stasm.dll
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/libproxy-stasm.dll
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=libproxy-stasm.dll.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/libproxy-stasm.dll.tar
---
+++ remote-client-webcam/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=remote-client-webcam
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/remote-client-webcam
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=remote-client-webcam.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/remote-client-webcam.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=remote-client-webcam
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/remote-client-webcam
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=remote-client-webcam.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/remote-client-webcam.tar
